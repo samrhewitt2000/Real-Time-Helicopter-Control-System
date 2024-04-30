11b28fc1ddbacdffd3be55c02930eb18fd6dd139
Caleb Westbury <cwe111@uclive.ac.nz>
2024-05-01 11:27:58 +1200

    Introduced update_ADC_val function into ADC.c

--- Milestone_1/ADC.c
+++ Milestone_1/ADC.c
@@ -138,3 +138,15 @@ initADC (void)
     ADCIntEnable(ADC0_BASE, 3);
 }
 
+//*****************************************************************************
+//
+// The code for calculating the ADC value
+//
+//*****************************************************************************
+int32_t
+update_ADC_val(circBuf_t *buffer, uint32_t buf_size)
+{
+    int32_t sum = 0;
+    sum = sum_CircBuf_vals (sum, &buffer, buf_size);
+    return (2 * sum + buf_size) / 2 / buf_size;
+}
